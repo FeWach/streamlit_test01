02c682b89fc124916984bd6f2458e85eeea9b24a
Felix Wach <34925362+FeWach@users.noreply.github.com>
2023-11-10 10:47:55 +0100

    Update popularitywithchatbot.py

--- popularitywithchatbot.py
+++ popularitywithchatbot.py
@@ -5,27 +5,6 @@ import numpy as np
 
 csv_url = "https://raw.githubusercontent.com/FeWach/streamlit_test01/main/sample_movies_2.csv"
 
-
-def calculate_popularity(df):
-    
-    ratings_count = df.groupby('movieId')['rating'].count()
-    
-    average_ratings = df.groupby('movieId')['rating'].mean()
-    
-    popularity_score = average_ratings * np.log1p(ratings_count)
-    
-    return popularity_score.to_dict()
-
-def popularity_recommender(df, num_recommendations):
-    
-    df['popularity_score'] = df['movieId'].map(calculate_popularity(df))
-    
-    sorted_movies = df.sort_values(by='popularity_score', ascending=False)
-    
-    recommended_movies = sorted_movies.drop_duplicates(subset='title').head(num_recommendations)
-
-    return recommended_movies[['movieId', 'title', 'genres']]
-
 def run_streamlit_app():
     
     df = pd.read_csv(csv_url) #df ok
@@ -58,25 +37,26 @@ def run_streamlit_app():
       recommended_movies = sorted_movies.drop_duplicates(subset='title').head(num_recommendations)
 
       return recommended_movies[['movieId', 'title', 'genres']]
-        if genre_choice in genre_map:
+        
+    if genre_choice in genre_map:
         selected_genre = genre_map[genre_choice]
         st.write(f"You have chosen {selected_genre}")
         top_movies = popularity_recommender(df, 15)
 
         a = top_movies[top_movies["genres"].str.contains(selected_genre)] 
 
-      if not a.empty:
+    if not a.empty:
             st.write("Here are your personal movie recommendations, Enjoy! 🍿")
             for movie_title in a['title']:
                 st.write(movie_title)
-      else:
+    else:
             st.write("No recommendations found now, sorry. 😕")
 
         continue_chat = st.radio("Do you want another recommendation?", ["Yes", "No"])
-      if continue_chat == "No":
+    if continue_chat == "No":
             st.write("Thanks for using the recommender! Have a great day! 👋")
             st.stop()
-      else:
+    else:
             st.write("Please type 1, 2, or 3 again. ⭐️")
 
 if __name__ == "__main__":
