e1421a6a46b967e481d64bfc57570634f98a94a3
Felix Wach <34925362+FeWach@users.noreply.github.com>
2023-11-10 11:17:12 +0100

    Update popularitywithchatbot.py

--- popularitywithchatbot.py
+++ popularitywithchatbot.py
@@ -22,44 +22,44 @@ def run_streamlit_app():
              selected_genre = genre_map[genre_choice]
              st.write(f"You have chosen {selected_genre}")
          
-                    def calculate_popularity(df):
+     def calculate_popularity(df):
     
-                          ratings_count = df.groupby('movieId')['rating'].count()
+               ratings_count = df.groupby('movieId')['rating'].count()
     
-                          average_ratings = df.groupby('movieId')['rating'].mean()
+               average_ratings = df.groupby('movieId')['rating'].mean()
     
-                          popularity_score = average_ratings * np.log1p(ratings_count)
+               popularity_score = average_ratings * np.log1p(ratings_count)
     
-                          return popularity_score.to_dict()
+               return popularity_score.to_dict()
 
-                    def popularity_recommender(df, num_recommendations):
+     def popularity_recommender(df, num_recommendations):
     
-                          df['popularity_score'] = df['movieId'].map(calculate_popularity(df))
+               df['popularity_score'] = df['movieId'].map(calculate_popularity(df))
     
-                          sorted_movies = df.sort_values(by='popularity_score', ascending=False)
+               sorted_movies = df.sort_values(by='popularity_score', ascending=False)
     
-                          recommended_movies = sorted_movies.drop_duplicates(subset='title').head(num_recommendations)
+               recommended_movies = sorted_movies.drop_duplicates(subset='title').head(num_recommendations)
 
-                          return recommended_movies[['movieId', 'title', 'genres']]
+               return recommended_movies[['movieId', 'title', 'genres']]
                
-               top_movies = popularity_recommender(df, 15)
+     top_movies = popularity_recommender(df, 15)
 
-               a = top_movies[top_movies["genres"].str.contains(selected_genre)] 
+     a = top_movies[top_movies["genres"].str.contains(selected_genre)] 
 
-               if not a.empty:
-                      st.write("Here are your personal movie recommendations, Enjoy! 🍿")
-                      for movie_title in a['title']:
-                          st.write(movie_title)
-               else:
-                      st.write("No recommendations found now, sorry. 😕")
+     if not a.empty:
+          st.write("Here are your personal movie recommendations, Enjoy! 🍿")
+          for movie_title in a['title']:
+               st.write(movie_title)
+     else:
+          st.write("No recommendations found now, sorry. 😕")
 
-               continue_chat = st.radio("Do you want another recommendation?", ["Yes", "No"])
+     continue_chat = st.radio("Do you want another recommendation?", ["Yes", "No"])
 
-               if continue_chat == "No":
-                      st.write("Thanks for using the recommender! Have a great day! 👋")
-                      st.stop()
-               else:
-                      st.write("Please type 1, 2, or 3 again. ⭐️")
+     if continue_chat == "No":
+          st.write("Thanks for using the recommender! Have a great day! 👋")
+          st.stop()
+     else:
+          st.write("Please type 1, 2, or 3 again. ⭐️")
 
 if __name__ == "__main__":
     run_streamlit_app()
